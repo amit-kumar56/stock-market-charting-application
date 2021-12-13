ab45ddc49813608929cf20e6a7937626582201aa
Amit <amit.ksingh@collegedekho.com>
2021-12-13 10:40:12 +0530

    2nd commit of StockExchange

--- src/main/java/com/example/Amit/StockExchangeAppApplication.java
+++ src/main/java/com/example/Amit/StockExchangeAppApplication.java
@@ -2,8 +2,10 @@ package com.example.Amit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class StockExchangeAppApplication {
 
 	public static void main(String[] args) {
