bbc98d198b6f61c5ebb9154829387a3d151aca1b
Amit <amit.ksingh@collegedekho.com>
2021-12-15 23:23:11 +0530

    3rd commit of StockExchange

--- src/main/java/com/example/Amit/controller/StockExchangeController.java
+++ src/main/java/com/example/Amit/controller/StockExchangeController.java
@@ -16,8 +16,9 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 @RestController
+@CrossOrigin(origins= "*")
 @RequestMapping("/stockExchanges")
 public class StockExchangeController
 {
