a998172e02d1b91d56c6a0dda9fd602d26bea8b3
Amit <amit.ksingh@collegedekho.com>
2021-12-16 10:44:50 +0530

    3rd commit

--- src/main/java/com/example/Amit/controller/StockPriceController.java
+++ src/main/java/com/example/Amit/controller/StockPriceController.java
@@ -12,6 +12,7 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins= "*")
 @RequestMapping("/stock-price-service/stockPrices")
 public class StockPriceController
 {
